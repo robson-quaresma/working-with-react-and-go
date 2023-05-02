779b2b47bad2e752faaafebbd246bb90d9b9482e
Robson Quaresma <robquaresma@gmail.com>
2023-05-02 18:31:15 -0300

    adding a route to our handlers

--- react-apps/go-movies-back-end/cmd/api/main.go
+++ react-apps/go-movies-back-end/cmd/api/main.go
@@ -24,8 +24,6 @@ func main() {
 
 	log.Println("Starting application on port", port)
 
-	http.HandleFunc("/", Hello)
-
 	// start a web server
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
