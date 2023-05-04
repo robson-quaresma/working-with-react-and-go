e7de618df9cba1923d7468ea1cdd0c6ea7986882
Robson Quaresma <robquaresma@gmail.com>
2023-05-04 16:20:45 -0300

    creating a handler to list movies by genre

--- react-apps/go-movies-back-end/cmd/api/handlers.go
+++ react-apps/go-movies-back-end/cmd/api/handlers.go
@@ -354,3 +354,19 @@ func (app *application) DeleteMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, http.StatusOK, resp)
 }
+
+func (app *application) AllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	genreID, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	movies, err := app.DB.AllMovies(genreID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, movies)
+}
